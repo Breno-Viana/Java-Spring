1d94177983805991932dd424489e6654e061e279
Breno-Viana <150757313+Breno-Viana@users.noreply.github.com>
2024-11-23 19:25:24 -0300

    att

---
+++ ApiRestAPP/src/test/java/br/com/apirest/ApiRestAPP/ApiRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apirest.ApiRestAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
