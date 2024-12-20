d0554ea0c0d5d3d6976f4181924f13df3ade0e54
Breno-Viana <brenoglauber.dev@gmail.com>
2024-12-20 16:07:48 -0300

    att

---
+++ DevBR-Tutorial_Jpa/emprestimo_app/src/main/java/com/emprestimo/aplicacao/emprestimo_app/EmprestimoAppApplication.java
@@ -0,0 +1,13 @@
+package com.emprestimo.aplicacao.emprestimo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoAppApplication.class, args);
+	}
+
+}
