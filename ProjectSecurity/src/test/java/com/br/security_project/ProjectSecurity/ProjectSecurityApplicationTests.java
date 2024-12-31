8902fb432e38cfc602995e7643a2cf2928631acf
Breno-Viana <brenoglauber.dev@gmail.com>
2024-12-31 18:26:57 -0300

    projeto treino de spring security

---
+++ ProjectSecurity/src/test/java/com/br/security_project/ProjectSecurity/ProjectSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.security_project.ProjectSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
